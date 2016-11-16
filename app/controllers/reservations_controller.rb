fd6653cd4b99ab5da684f8f3b0129ecffdeaff49
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-11-16 14:29:29 -0500

    Created edit, update, and destroy routes for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,8 +4,32 @@ class ReservationsController < ApplicationController
   end
 
   def show
+    @reservation = Reservation.find(params["id"])
   end
 
   def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+    @reservation = Reservation.find(params["id"])
+  end
+
+  def update
+    @reservation = Reservation.find(params["id"])
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Reservation.find(params["id"]).destroy
+    redirect_to reservations_path
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:name, :due_date, :book_id, :overdue)
   end
 end
--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -11,13 +11,29 @@ class ReservationsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get new_reservation_path
     assert_response :success
-    assert_match(/Create a reservation/, response.body)
+    assert_match(/New Book Reservation/, response.body)
   end
 
   test "should get show" do
     get reservation_path(reservations(:one).id)
     assert_response :success
     assert_match(/Allie/, response.body)
+  end
+
+  test "can get edit form reservation" do
+    get edit_reservation_path(reservations(:one).id)
+    assert_response :success
+    assert_match(/Edit/, response.body)
+  end
+
+  test "can update reservation" do
+    patch reservation_path(reservations(:one).id), params: { reservation: { name: "Frenchy McGee" } }
+    assert_equal "Frenchy McGee", Reservation.find(reservations(:one).id).name
+  end
+
+  test "can delete reservation" do
+    delete reservation_path(reservations(:one).id)
+    refute Reservation.find_by(id: reservations(:one).id)
   end
 
 end
